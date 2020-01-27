8cba0cc40857aaccc780eed9bc3e2f3d7a0cbac3
Michael Loughlin <mike@loughlin.dev>
2020-01-27 22:49:13 +0000

    Add support for Clojure primitive values (used to be string-only).

---
+++ src/bitcack/io.clj
@@ -0,0 +1,86 @@
+(ns bitcack.io
+  (:import [java.io RandomAccessFile FileOutputStream DataOutputStream DataInputStream])
+  (:require [clojure.java.io :as jio]
+            [taoensso.nippy :as nippy]))
+
+
+(defn read-value-at [file offset]
+ (with-open [raf (doto (RandomAccessFile. file "r")
+                   (.seek offset))]
+     (let [ba-len (.readInt raf)
+           ba (byte-array ba-len)]
+       (.readFully raf ba)
+       ba)))
+
+(comment
+  (read-value-at "C:\\temp\\db\\0" 0))
+
+(defn append-bytes! [file ^bytes byte-array]
+  (let [ba-len (alength byte-array)]
+    (with-open [out (DataOutputStream. (FileOutputStream. file true))]
+        (.writeInt out ba-len)
+        (.write out byte-array))))
+
+
+(defn find-seg-files-in [dir]
+  (let [dir (jio/file dir)]
+    (if (.isDirectory dir)
+      (->> (file-seq dir)
+           (filter #(.isFile %))
+           (filter #(neg? (.lastIndexOf (.getName %) ".")))) ; segment files have no extension
+      nil)))
+
+
+(defn file-name [path]
+  (-> path
+      jio/file
+      .toPath
+      .getFileName
+      str))
+
+
+(defn file-length [path]
+  (-> path
+      jio/file
+      .length))
+
+
+(defn join-paths [& paths]
+  (str
+    (java.nio.file.Paths/get
+                             (first paths)
+                             (into-array (rest paths)))))
+
+(defn rename-file
+  "Renames a file in the same folder.
+  Args:
+  source (file)
+  rename-fn (function that takes the full source file name, returns the new file name)
+  copy-option :replace, :copy-attr, :atomic-move"
+  [source rename-fn copy-option]
+  (let [copy-options {:replace "REPLACE_EXISTING"
+                      :copy-attr "COPY_ATTRIBUTES"
+                      :atomic-move "ATOMIC_MOVE"}
+        dest (rename-fn (.getFileName source))]
+    (java.nio.file.Files/move
+      (.toPath source)
+      (.toPath (jio/file (join-paths (.getParent source) dest)))
+      (into-array java.nio.file.CopyOption [(java.nio.file.StandardCopyOption/valueOf (get copy-options copy-option))]))))
+
+
+(defn- safe-read-int [dis]
+  (try
+    (.readInt dis)
+    (catch java.io.EOFException e nil)))
+
+
+(defn map-segment [f file]
+  (letfn [(cack-seq [dis]
+            (when-let [len (safe-read-int dis)]
+              (let [ba (byte-array len)]
+                (.readFully dis ba)
+                (cons ba (lazy-seq (cack-seq dis))))))]
+    (with-open [dis (DataInputStream. (jio/input-stream (jio/file file)))]
+      (into () (map f (cack-seq dis))))))
+
+(comment (map-segment nippy/thaw "C:\\temp\\db\\0"))
--- src/bitcack/serialisation.clj
+++ src/bitcack/serialisation.clj
@@ -1,10 +1,40 @@
 (ns bitcack.serialisation
-  (:require [clojure.string :as str]))
+  (:require [taoensso.nippy :as nippy]))
 
 
-(defn serialize [key value]
-  (str key "," value (System/lineSeparator)))
+(defn serialize
+  ([kvp]
+   (nippy/freeze kvp))
+  ([key value]
+   (nippy/freeze [key value])))
+
+(comment
+  (serialize "hello" "my-value")
+  (serialize ["key" "value pair"]))
 
 
 (defn deserialize [input]
-  (vec (str/split input #",")))
+  (nippy/thaw input))
+
+
+(comment
+  (let [bytes (serialize "ah" 10001)]
+    (deserialize bytes)))
+
+
+(defn- serde-eq [input]
+  (assert (= (-> input
+               serialize
+               deserialize)
+             input)))
+
+
+(comment (run! serde-eq [["key" :keywordabc]
+                         ["key" ::namespacedkw]
+                         ["key" 100011]
+                         ["key" 10000000000001247237237723474327423234234]
+                         ["key" "my-string"]
+                         ["keyyyy" {:my :map}]
+                         ["key" [1 2 :a "yarp"]]
+                         ["key" #{:a :b :set-test}]
+                         ["bool-key" false]]))
